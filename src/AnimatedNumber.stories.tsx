577032899824555ca826af81d26490bac2aa445f
Benjamin <eunvanz@naver.com>
2022-02-06 04:37:39 +0900

    feat: add AnimatedNumber component

--- src/AnimatedNumber.stories.tsx
+++ src/AnimatedNumber.stories.tsx
@@ -1,17 +1,81 @@
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
-import AnimatedNumberItem from "./AnimatedNumberItem";
+import { useCallback, useState } from "react";
+import { Button, Switch, TextField } from "@mui/material";
+import { random } from "lodash-es";
+import AnimatedNumber from "./AnimatedNumber";
 
 export default {
-  title: "components/AnimatedNumberItem",
-  component: AnimatedNumberItem,
+  title: "components/AnimatedNumber",
+  component: AnimatedNumber,
   args: {
-    number: 3,
-    size: 18,
+    number: 31290827,
+    size: 20,
+    duration: 200,
   },
-} as ComponentMeta<typeof AnimatedNumberItem>;
+} as ComponentMeta<typeof AnimatedNumber>;
 
-const Template: ComponentStory<typeof AnimatedNumberItem> = (args) => (
-  <AnimatedNumberItem {...args} />
+const Template: ComponentStory<typeof AnimatedNumber> = (args) => (
+  <AnimatedNumber {...args} />
 );
 
 export const Default = Template.bind({});
+
+export const WithController = () => {
+  const [number, setNumber] = useState(random(0, 100000000));
+  const [hasComma, setHasComma] = useState(false);
+  const [size, setSize] = useState(24);
+  const [duration, setDuration] = useState(200);
+
+  const increaseNumber = useCallback(() => {
+    setNumber((number) => number + random(0, 100000000));
+  }, []);
+
+  const decreaseNumber = useCallback(() => {
+    setNumber((number) => number - random(0, 100000000));
+  }, []);
+
+  return (
+    <>
+      <AnimatedNumber
+        number={number}
+        hasComma={hasComma}
+        size={size}
+        duration={duration}
+      />
+      <div style={{ marginTop: 10 }}>
+        <Button variant="contained" size="small" onClick={increaseNumber}>
+          Increase Number
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          style={{ marginLeft: 5 }}
+          onClick={decreaseNumber}
+        >
+          Decrease Number
+        </Button>
+        <div style={{ display: "flex", alignItems: "center", marginTop: 10 }}>
+          <Switch onChange={(_, checked) => setHasComma(checked)} /> Comma
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <TextField
+            type="number"
+            value={size}
+            size="small"
+            label="Size"
+            onChange={(e) => setSize(Number(e.target.value))}
+          />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <TextField
+            type="number"
+            value={duration}
+            size="small"
+            label="Duration"
+            onChange={(e) => setDuration(Number(e.target.value))}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
--- src/AnimatedNumber.tsx
+++ src/AnimatedNumber.tsx
@@ -1,9 +1,51 @@
+import { Fragment, useMemo } from "react";
+import "./AnimatedNumber.scss";
+import AnimatedNumberItem from "./AnimatedNumberItem";
+
 export interface AnimatedNumberProps {
   number: number;
   size: number;
-  hasComma: boolean;
+  hasComma?: boolean;
+  duration?: number;
 }
 
-export const AnimatedNumber = ({ number, size, hasComma }: AnimatedNumberProps) => {
-  return <></>;
+const AnimatedNumber = ({ number, size, hasComma, duration }: AnimatedNumberProps) => {
+  const numberArray = useMemo(() => {
+    return String(number).split("");
+  }, [number]);
+
+  return (
+    <div className="ReactAwesomeAnimatedNumber" style={{ height: size }}>
+      {numberArray.map((number, index) => (
+        <Fragment key={index}>
+          {hasComma && (numberArray.length - index) % 3 === 0 && index !== 0 && (
+            <div
+              className="ReactAwesomeAnimatedNumber__text"
+              style={{
+                fontSize: size,
+                height: size,
+              }}
+            >
+              ,
+            </div>
+          )}
+          {number === "-" ? (
+            <div
+              className="ReactAwesomeAnimatedNumber__text"
+              style={{
+                fontSize: size,
+                height: size,
+              }}
+            >
+              -
+            </div>
+          ) : (
+            <AnimatedNumberItem number={Number(number)} size={size} duration={duration} />
+          )}
+        </Fragment>
+      ))}
+    </div>
+  );
 };
+
+export default AnimatedNumber;
