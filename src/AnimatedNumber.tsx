7fab5f549554f6b3333df2bea5aec1ff20eaa954
Benjamin <eunvanz@naver.com>
2022-02-06 13:30:43 +0900

    feat: feature for decimal

--- src/AnimatedNumber.tsx
+++ src/AnimatedNumber.tsx
@@ -23,6 +23,18 @@ const AnimatedNumber = ({
     return String(value).split("");
   }, [value]);
 
+  const isMinus = useMemo(() => {
+    return numberArray[0] === "-";
+  }, [numberArray]);
+
+  const decimalPointIndex = useMemo(() => {
+    return numberArray.findIndex((value) => value === ".");
+  }, [numberArray]);
+
+  const decimalLength = useMemo(() => {
+    return decimalPointIndex > -1 ? numberArray.length - decimalPointIndex : 0;
+  }, [numberArray, decimalPointIndex]);
+
   return (
     <div
       className={
@@ -34,24 +46,17 @@ const AnimatedNumber = ({
       {...restProps}
     >
       {numberArray.map((number, index) => {
-        const isMinus = numberArray[0] === "-";
+        const isInt = decimalLength ? index < decimalPointIndex : true;
+        const isCommaNeeded =
+          hasComma &&
+          isInt &&
+          (numberArray.length - decimalLength - index) % 3 === 0 &&
+          index !== 0 &&
+          (isMinus ? index !== 1 : true);
+
         return (
           <Fragment key={index}>
-            {hasComma &&
-              (numberArray.length - (isMinus ? 3 : 0) - index) % 3 === 0 &&
-              index !== 0 &&
-              (isMinus ? index !== 1 : true) && (
-                <div
-                  className="ReactAwesomeAnimatedNumber__text"
-                  style={{
-                    fontSize: size,
-                    height: size,
-                  }}
-                >
-                  ,
-                </div>
-              )}
-            {number === "-" ? (
+            {isCommaNeeded && (
               <div
                 className="ReactAwesomeAnimatedNumber__text"
                 style={{
@@ -59,7 +64,18 @@ const AnimatedNumber = ({
                   height: size,
                 }}
               >
-                -
+                ,
+              </div>
+            )}
+            {isNaN(Number(number)) ? (
+              <div
+                className="ReactAwesomeAnimatedNumber__text"
+                style={{
+                  fontSize: size,
+                  height: size,
+                }}
+              >
+                {number}
               </div>
             ) : (
               <AnimatedNumberItem
